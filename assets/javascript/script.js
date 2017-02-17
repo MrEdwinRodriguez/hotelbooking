0bfae2ce6c3c132a9ab9d416147d1ab23460760d
MrEdwinRodriguez <mredwinrodriguez@gmail.com>
2017-02-16 22:03:51 -0500

    dynamicly created select

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,8 +13,23 @@ function getLocation(place) {
         var responseArray = response._embedded['city:search-results']
         var responseArrayLength = responseArray.length;
 
+        // var q = document.getElementById("listCities");
+        // var x = document.createElement("SELECT");
+        // x.setAttribute("id", "mySelect");
+        // //document.body.appendChild(x);
+        // q.add(x)
+
+        // var z = document.createElement("option");
+        // z.setAttribute("value", "volvocar");
+        // var t = document.createTextNode("Volvo");
+        // z.appendChild(t);
+        // document.getElementById("mySelect").appendChild(z);
+
+
+document.getElementById("listCities").style.visibility = "visible"
+
         for (i = 0; i < responseArrayLength; i++) {
-        	console.log('hello')
+            console.log('hello')
             var cityChoices = response._embedded['city:search-results'][i].matching_alternate_names[0].name;
             // return
             console.log(cityChoices)
@@ -36,7 +51,7 @@ function showList(city) {
 $(document).ready(function() {
 
     $('#selectLocation').on('click', function() {
-    // $('#selectLocation').keyup(function() {
+        // $('#selectLocation').keyup(function() {
         // Grab the location
         var location = $('#location').val().trim();
         console.log(location);
